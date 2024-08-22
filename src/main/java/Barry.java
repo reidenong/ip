8c62429dcf5ca9c14a404f321cf97af010d2b02a
reidenong <reidenong02@gmail.com>
2024-08-22 12:43:03 +0800

    Level 0: Replaced Duke with Barry.

---
+++ src/main/java/Barry.java
@@ -0,0 +1,6 @@
+public class Barry {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Barry.\nWhat can I do for you?\n");
+        System.out.println("Bye-Bye.");
+    }
+}
