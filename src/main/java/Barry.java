232fda639fcf3e19e019be39ba6680f3b37c0fea
reidenong <reidenong02@gmail.com>
2024-08-22 14:07:57 +0800

    Level 3: Mark, unmark as done

--- src/main/java/Barry.java
+++ src/main/java/Barry.java
@@ -3,7 +3,35 @@ import java.util.Scanner;
 
 public class Barry {
     private boolean alive;
-    private ArrayList<String> tasks;
+    private ArrayList<Task> tasks;
+
+    private class Task {
+        private boolean completed;
+        private String description;
+
+        public Task(String description) {
+            this.description = description;
+        }
+
+        public void mark() {
+            this.completed = true;
+        }
+
+        public void unmark() {
+            this.completed = false;
+        }
+
+        public boolean isDone() {
+            return this.completed;
+        }
+
+        @Override
+        public String toString() {
+            String output = (this.completed ? "[X] " : "[ ] ");
+            return output + this.description;
+        }
+
+    }
     
     public Barry() {
         this.alive = false;
@@ -40,21 +68,48 @@ public class Barry {
             return;
         }
 
+        if (input.startsWith("mark")) {
+            this.action("mark", input.substring(5));
+            return;
+        }
+
+        if (input.startsWith("unmark")) {
+            this.action("unmark", input.substring(7));
+            return;
+        }
+
         this.action("addtask", input);
     }
 
     private void action(String command, String data) {
         if (command.equals("addtask")) {
-            this.tasks.add(data);
+            this.tasks.add(new Task(data));
             this.speak("task added: " + data, true);
             return;
         }
+
+        if (command.equals("mark")) {
+            int idx = Integer.parseInt(data) - 1;
+            this.tasks.get(idx).mark();
+            this.speak("I've marked this task as done:", false);
+            this.speak(this.tasks.get(idx).toString(), true);
+            return;
+        }
         
+        if (command.equals("unmark")) {
+            int idx = Integer.parseInt(data) - 1;
+            this.tasks.get(idx).unmark();
+            this.speak("I've unmarked this task:", false);
+            this.speak(this.tasks.get(idx).toString(), true);
+            return;
+        }
+
         if (command.equals("list")) {
             int N = this.tasks.size();
             for (int i = 0; i < N; i++) {
-                this.speak((i + 1) + ": " + this.tasks.get(i), i == N - 1);
+                this.speak((i + 1) + ". " + this.tasks.get(i).toString(), i == N - 1);
             }
+            return;
         }
     }
 
