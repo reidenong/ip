d626b445730822b3faa1b87fbda07a740c92eadd
reidenong <reidenong02@gmail.com>
2024-09-25 14:34:08 +0800

    Added comments to the UI class
    The UI class lacked comments and this made it decently difficult
    for one to see what was going on in the code. Some annotations
    on the methods and what they meant would really help for future
    maintenance or refactoring.

--- src/main/java/barry/Ui.java
+++ src/main/java/barry/Ui.java
@@ -1,33 +1,41 @@
 package barry;
+
 import java.util.Scanner;
 
 /**
- * The Ui class manages the User Command line interface that the user uses to communicate with Barry.
+ * The Ui class manages the User Command line interface that the user uses to
+ * communicate with Barry.
  */
 public class Ui {
     private Scanner scanner;
 
+    // UI Constructor
     public Ui() {
         this.scanner = new Scanner(System.in);
     }
 
+    // Takes in the next command from shell
     public String readCommand() {
         return scanner.nextLine();
     }
 
+    // Defines a welcome message
     public String showWelcomeMessage() {
-        return("Hello! I'm Barry.\nWhat can I do for you?\n");
+        return ("Hello! I'm Barry.\nWhat can I do for you?\n");
     }
 
+    // Defines a goodbye message
     public String showGoodbyeMessage() {
-        return("Bye. See you soon!\n");
+        return ("Bye. See you soon!\n");
     }
 
+    // Shows the error message
     public String showMessage(String message) {
-        return(message + "\n");
+        return (message + "\n");
     }
 
+    // Shows the error message
     public String showLoadingError() {
-        return("An error occurred while loading tasks from the file.\n");
+        return ("An error occurred while loading tasks from the file.\n");
     }
 }
